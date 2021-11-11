ab50e2b0cc1c3b4e742b74f03ad9d38d962b15ab
Toni <easyrobotics2@gmail.com>
2021-11-11 10:57:11 +0100

    Fin window object

---
+++ window/js/main.js
@@ -0,0 +1,48 @@
+//Objeto window
+//window.alert("hello");
+//prompt("Escribe:")
+
+// if (confirm("Estas seguro?")) {
+//   console.log("Siiiii!");
+// } else {
+//   console.log("NOOOOO");
+// }
+
+let val;
+
+val = window.outerHeight;
+val = window.outerWidth;
+
+val = window.innerHeight;
+val = window.innerWidth;
+
+//Scroll
+val = window.scrollY;
+val = window.scrollX;
+
+//Location
+val = window.location;
+val = window.location.hostname;
+val = window.location.port;
+val = window.location.href;
+
+//Redirección
+// window.location.href = "http://google.es";
+// window.location.reload();
+
+//History
+// window.history.go(1)
+// val = window.history.length;
+
+//window navigator
+
+val = window.navigator;
+val = window.navigator.appName;
+val = window.navigator.appVersion;
+val = window.navigator.userAgent;
+val = window.navigator.platform;
+val = window.navigator.vendor;
+val = window.navigator.language;
+
+val = window.document;
+console.log(val);
