c056264b2d3b014e14411ec93bfd2203443f4061
Antoni Moragues Font <easyrobotics2@gmail.com>
2021-11-29 14:30:01 +0100

    ejemplo closure

--- prototypes/js/app_4.js
+++ prototypes/js/app_4.js
@@ -1,41 +1,42 @@
 //Constructor
 function Persona(nombre, apellidos) {
-  let edad = 0;//propiedad privada
+  let edad = 0; //propiedad privada
 
   //Propiedades publicas
   this.nombre = nombre;
   this.apellidos = apellidos;
-  
+
   //metodo privado
-   function envejecer(){
+  const self = this;
+  function envejecer() {
     edad++;
+    //console.log(self.nombre)
   }
 
-  setInterval(envejecer, 1000);
+  window.setInterval(envejecer, 1000);
 
   //Métodos públicos
-  this.getEdad = function(){
-     return edad;
-  }
+  this.getEdad = function () {
+    return edad;
+  };
 }
 //Método publico común a todos  los objetos de la clase Persona
-Persona.prototype.saludar = function(){
-  return `Hola me llamo ${this.nombre} ${this.apellidos}`
-}
+Persona.prototype.saludar = function () {
+  return `Hola me llamo ${this.nombre} ${this.apellidos}`;
+};
 
+const marisol = new Persona("Pepa", "Flores");
+const marisol2 = new Persona("Pepa", "Flores");
 
-const marisol = new Persona('Pepa', 'Flores')
-const marisol2 = new Persona('Pepa', 'Flores')
-
-console.log(marisol)
-console.log(marisol.getEdad())
+console.log(marisol);
+console.log(marisol.getEdad());
 
 //Método estatico
-Persona.sumar = function(a, b){
+Persona.sumar = function (a, b) {
   return a + b;
-}
+};
 //Propiedad estatica
 Persona.existe = true;
 
-console.log(Persona.existe)
-console.log(Persona.sumar(2,4))
+console.log(Persona.existe);
+console.log(Persona.sumar(2, 4));
